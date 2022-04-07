350406138eaf8da010e44e24b265d032d251334d
Gabriel Avelino <48573662+gabrielavelino@users.noreply.github.com>
2022-04-07 11:23:27 -0300

    Adicionando testes para o campo NUMBER

--- src/test/java/org/jabref/model/entry/field/StandardFieldTest.java
+++ src/test/java/org/jabref/model/entry/field/StandardFieldTest.java
@@ -1,13 +1,43 @@
 package org.jabref.model.entry.field;
 
+import org.jabref.logic.importer.fileformat.endnote.Volume;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class StandardFieldTest {
 
+/*
     @Test
-    void fieldsConsideredEqualIfSame() {
-        assertEquals(StandardField.TITLE, StandardField.TITLE);
+    void fieldNumberOnlyNumbers() {
+        String nome = StandardField.NUMBER.getName("2");
+        assertTrue(Boolean.parseBoolean("2"),nome);
+    } */
+
+
+    @Test
+    void fieldNumberOnlyNumbers() {
+       String nome = String.valueOf(StandardField.NUMBER.numberInteiros("2"));
+        assertEquals("2", nome);
     }
+
+    @Test
+    void fieldNumberCannotTakeSpecialCharacters() {
+        String nome = String.valueOf(StandardField.NUMBER.numberInteiros("#teste"));
+        assertFalse(Boolean.parseBoolean("#teste"),nome);
+    }
+
+    @Test
+    void fieldNumberCannotTakeSymbols() {
+        String nome = String.valueOf(StandardField.NUMBER.numberInteiros("abd!"));
+        assertFalse(Boolean.parseBoolean("abd!"),nome);
+
+    }
+
+    @Test
+    void fieldNumberManyNumbers() {
+        String nome = String.valueOf(StandardField.NUMBER.numberInteiros("2341"));
+        assertEquals("2341", nome);
+    }
+
 }
